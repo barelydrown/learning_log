1748d9273f7d2f29db7885bb6c3c79989c148053
barelydrown <jooomanfirst@yandex.ru>
2021-09-04 17:02:10 +0300

    main page, topics, entries, editing

---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Тема, которую изучает пользователь"""
+    text = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Информация, изученная пользователем по конкретной теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        if len(self.text) > 50:
+            return f'{self.text[:50]}...'
+        else:
+            return f'{self.text}'
